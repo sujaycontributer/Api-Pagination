df67970b5f0e1f4a32118c4a7d2954dcfe42f515
Sujay Ghosh <141671791+sujaycontributer@users.noreply.github.com>
2025-07-21 21:53:55 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import mongoose, { mongo } from "mongoose";
 const app = express();
 app.use(cors())
 
+// ---------------------------Pagination from backend-----------------------
 app.get("/products", async (req, res) => {
   const page:number = parseInt(req.query.page as string) || 1; // get the page number from the frontend
   const limit:number = parseInt(req.query.limit as string) || 5; // content per page
